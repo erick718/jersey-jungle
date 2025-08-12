dae067f8282c9f95757d109cdf1d3cbc124af2cc
“Erick” <144264479+erick718@users.noreply.github.com>
2025-08-12 13:59:43 -0400

    commit

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 
 //configure app
-let port = 3000;
+let port = 2000;
 let host = 'localhost';
-let url = 'mongodb://localhost:27017/project4'
+let url = 'mongodb://localhost:27017'
 app.set('view engine', 'ejs');
 const mongUri = 'mongodb+srv://admin:admin123@cluster0.kogrf.mongodb.net/project5?retryWrites=true&w=majority&appName=Cluster0';
 
